f9967b2600decc2fa9484043f3c751daf0bc2283
Dan Marzullo <dan.marzullo@gmail.com>
2017-09-28 22:08:41 -0400

    Initial Commit

---
+++ Assets/Scripts/Select.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+[RequireComponent(typeof(Collider))]
+public class Select : MonoBehaviour {
+
+    // Use this for initialization
+	void Start () {
+        SetGazedAt(false);
+	}
+
+    public void SetGazedAt(bool gazedAt) {
+        GetComponent<Renderer>().material.color = gazedAt ? Color.green : Color.red;
+    }
+
+    public void LoadScene(string Scene)
+    {
+        GetComponent<Renderer>().material.color = Color.blue;
+        SceneManager.LoadScene(Scene, LoadSceneMode.Single);
+    }
+	
+}
