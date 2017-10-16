0743044ae0fbed27aa6491259e7c84bf276b981e
Dan Marzullo <dmarzull@its.jnj.com>
2017-10-15 22:24:27 -0400

    Garage Mostly Done

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	public Transform camera;
+
+	public float speed = 3.0f;
+
+	private CharacterController cc;
+
+	// Use this for initialization
+	void Start () {
+		cc = GetComponent<CharacterController>();	
+	}
+	
+	// Update is called once per frame
+	public void Update () {
+		if (Input.GetMouseButton (0)) {
+			Vector3 forward = camera.TransformDirection(Vector3.forward);
+			cc.SimpleMove (forward * speed);
+		}
+	}
+}
--- Assets/Scripts/Select.cs
+++ Assets/Scripts/Select.cs
@@ -6,18 +6,24 @@ using UnityEngine.SceneManagement;
 [RequireComponent(typeof(Collider))]
 public class Select : MonoBehaviour {
 
+	public Animator animator;
+
     // Use this for initialization
 	void Start () {
         SetGazedAt(false);
+
+		animator = GetComponent<Animator> ();
+		Debug.Log (animator);
 	}
 
     public void SetGazedAt(bool gazedAt) {
-        GetComponent<Renderer>().material.color = gazedAt ? Color.green : Color.red;
+//        GetComponent<Renderer>().material.color = gazedAt ? Color.green : Color.red;
+		animator.SetBool ("isGazedAt", gazedAt);
     }
 
     public void LoadScene(string Scene)
     {
-        GetComponent<Renderer>().material.color = Color.blue;
+//        GetComponent<Renderer>().material.color = Color.blue;
         SceneManager.LoadScene(Scene, LoadSceneMode.Single);
     }
 	
